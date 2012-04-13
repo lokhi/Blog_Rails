13dcda425a8ead45a40e648f67150ec860e56108
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-04-13 20:06:47 +0200

    oubli d'un fichier

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,13 +1,6 @@
 require 'spec_helper'
 
 describe "posts/new.html.erb" do
-  before(:each) do
-    # on positionne les variables que va recevoir la vue
-    assign(:post, stub_model(Post,
-      :title => "",
-      :body => ""
-    ).as_new_record)
-  end
   it "renders new post form" do
     render
     rendered.should have_selector("form[action='/posts']")
