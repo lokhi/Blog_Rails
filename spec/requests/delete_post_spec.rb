e90a805ed6efd4d026d44ab641d0b624ab19974c
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-04-18 12:21:40 +0200

    correction erreur d'edtion du mauvais fichier

--- spec/requests/delete_post_spec.rb
+++ spec/requests/delete_post_spec.rb
@@ -6,12 +6,12 @@ describe 'delete_post' do
   end
     
   it "should print a link to delete the post on the posts page" do
-    visit show_post_path(@post)
-    page.should have_link('Delete' , :href => delete_post_path(@post.id))
+    visit posts_path(@post)
+    page.should have_link('Delete' , :href => delete_post_path(@post))
   end
   
   it "should not print the post" do
-    visit show_post_path(@post)
+    visit posts_path(@post)
     click_link "Delete"
     current_path.should == posts_path
     page.should have_no_content(@post.title)
