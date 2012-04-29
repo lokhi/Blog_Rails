ab9952b87cb756970409dfdd1ee182d282e7574b
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-04-29 15:28:00 +0200

    Debut scénario ajout du bbcode mais probleme de test d'utilisation :sob:

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
+require 'bb-ruby'
 class Post < ActiveRecord::Base
   has_many :comments, :dependent => :delete_all
+  
+  def printBody
+    body.bbcode_to_html_with_formatting.html_safe
+  end
+  
+  
+  def printDescription
+    if body.length <= 300
+      body.bbcode_to_html_with_formatting.html_safe
+    else
+      text="#{body[0..299]} ..."
+      text.bbcode_to_html_with_formatting.html_safe
+    end
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,49 @@
 require 'spec_helper'
-
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+      @p=Post.new
+      @p.title="Test"
+      @p.body="une chaine avec [b]un mot en gras[/b]"
+      @p.save
+  end
+  describe "printBody" do
+    it "should use use the bbcode_to_html_with_formatting method" do
+      @p.body.should_receive(:bbcode_to_html_with_formatting).and_return('<p>une chaine avec <strong>un mot en gras</strong></p>')
+      @p.printBody
+    end
+    
+    
+    it "should use html_safe method" do
+      @p.body.bbcode_to_html_with_formatting.should_receive(:html_safe)
+      @p.printBody
+    end
+  end
+  describe "PrintDescription" do
+    it "should check if the length of body is lower than 300" do
+      @p.body.should_receive(:length)
+      @p.printDescription
+    end
+    
+    context "the body length is lower or egal at 300" do
+      it "should use  the bbcode_to_html_with_formatting method" do
+        @p.body.should_receive(:bbcode_to_html_with_formatting)
+        @p.printDescription
+      end
+    end
+    
+    context "the body length is > 300" do
+      before(:each) do
+        @p.body="une chaine avec [b]un mot en gras[/b] une chaine avec [b]un mot en gras[/b] une chaine avec [b]un mot en gras[/b] une chaine avec [b]un mot en gras[/b] une chaine avec [b]un mot en gras[/b] une chaine avec [b]un mot en gras[/b] une chaine avec [b]un mot en gras[/b] une chaine avec [b]un mot en gras[/b]"
+        @p.save
+      end
+      it "should take the 300 first chars" do
+        @p.body.should_receive(:[])
+        @p.printDescription
+      end
+      
+      it "should return a string close by ..." do
+        @p.printDescription =~ /( ...)$/
+      end
+    end 
+  end
 end
