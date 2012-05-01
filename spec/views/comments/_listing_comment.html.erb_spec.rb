3050829d3c22e46ec64f6b825a102b9fcdb88c29
Louis Perello <louis.perello@etu.enseeiht.fr>
2012-05-01 15:17:02 +0200

    Regle d'affichage des liens d'administration

--- spec/views/comments/_listing_comment.html.erb_spec.rb
+++ spec/views/comments/_listing_comment.html.erb_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe "/comments/_form_comment" do
   before(:each) do
+    session["current_user"] = "toto"
     @p = stub_model(Post, :title => "title1",:body => "Content test")
     @c = @p.comments.create({:name => "louis",:content => "my comment"})
     assign(:post,@p)
@@ -11,11 +12,25 @@ describe "/comments/_form_comment" do
     rendered.should =~ /louis/
     rendered.should =~ /my comment/
   end
-  
- it "should renders the link to delete a comment" do
-  render :partial => "comments/listing_comment"
-  rendered.should have_link('Delete', :href => delete_comment_path(@p.id,@c.id))
+ 
+ context "the user is connected " do
+   it "should renders the link to delete a comment" do
+    session["current_user"] = "toto"
+    render :partial => "comments/listing_comment"
+    rendered.should have_link('Delete', :href => delete_comment_path(@p.id,@c.id))
+   end
+ 
  end
+ 
+ context "the user is not connected " do
+   it "should not renders the link to delete a comment" do
+   session["current_user"] = nil
+    render :partial => "comments/listing_comment"
+    rendered.should_not have_link('Delete', :href => delete_comment_path(@p.id,@c.id))
+   end
+ 
+ end
+ 
  
  
  it "should renders the anchor to acces to the comment" do
